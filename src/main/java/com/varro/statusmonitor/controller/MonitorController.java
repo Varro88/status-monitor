b80ffd376a2e8cfc044e65845f8c2cd160fb7d4d
Varro88 <varro.postbox@gmail.com>
2025-04-19 16:05:22 +0300

    Added legacy update via GET

--- src/main/java/com/varro/statusmonitor/controller/MonitorController.java
+++ src/main/java/com/varro/statusmonitor/controller/MonitorController.java
@@ -6,11 +6,9 @@ import com.varro.statusmonitor.model.Status;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @Slf4j
@@ -33,4 +31,15 @@ public class MonitorController {
     public ResponseEntity<List<Status>> getStatuses() {
         return new ResponseEntity<>(monitorService.getStatuses(), HttpStatus.OK);
     }
+
+    @GetMapping("/monitor/legacy")
+    public ResponseEntity<String> addStatusLegacy(@RequestParam(value = "source") String source,
+                                                  @RequestParam(value = "timestamp", required = false) String timestamp) {
+        ZonedDateTime zonedDateTime = timestamp == null ? ZonedDateTime.now()
+                : ZonedDateTime.parse(timestamp.replace(" ","+"));
+        SimpleResponse resp = monitorService.addStatus(new Status(source, zonedDateTime));
+        return new ResponseEntity<>(resp.getMessage(), resp.getHttpStatus());
+    }
+
+
 }
--- src/main/java/com/varro/statusmonitor/model/Status.java
+++ src/main/java/com/varro/statusmonitor/model/Status.java
@@ -1,10 +1,12 @@
 package com.varro.statusmonitor.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.ZonedDateTime;
 
 @Data
+@AllArgsConstructor
 public class Status {
     private String source;
     private ZonedDateTime timestamp;
